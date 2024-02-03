abbfb9810be4b0d821946bd4b0885ce4b3cf3afa
Benedict Halim <benedicthalim1@gmail.com>
2024-02-03 15:23:51 +0700

    migrate from create-react-app to vite

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,24 +1,27 @@
 /* global google */
 import "./Navbar.css";
 import imgLogo from "../assets/navbar_main_logo.png";
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
-import { useUser } from '../UserContext';
+import { useUser } from "../UserContext";
 
 const Navbar = () => {
   const { user, updateUser } = useUser();
   const [isSidebarVisible, setIsSidebarVisible] = useState(false);
 
-  const handleCallbackResponse = useCallback(response => {
-    var userObject = jwtDecode(response.credential);
-    console.log(userObject);
-    updateUser(userObject);
-    document.getElementById("signInSidebar").hidden = true;
-    document.getElementById("signInMain").hidden = true;
-  }, [updateUser]);
+  const handleCallbackResponse = useCallback(
+    (response) => {
+      var userObject = jwtDecode(response.credential);
+      console.log(userObject);
+      updateUser(userObject);
+      document.getElementById("signInSidebar").hidden = true;
+      document.getElementById("signInMain").hidden = true;
+    },
+    [updateUser]
+  );
 
-  function handleSignOut(event){
+  function handleSignOut(event) {
     updateUser({});
     document.getElementById("signInSidebar").hidden = false;
     document.getElementById("signInMain").hidden = false;
@@ -27,10 +30,10 @@ const Navbar = () => {
   useEffect(() => {
     if (window.google && window.google.accounts) {
       google.accounts.id.initialize({
-        client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-        callback: handleCallbackResponse
+        client_id: import.meta.env.VITE_REACT_APP_GOOGLE_CLIENT_ID,
+        callback: handleCallbackResponse,
       });
-  
+
       google.accounts.id.renderButton(
         document.getElementById("signInSidebar"),
         {
@@ -39,27 +42,24 @@ const Navbar = () => {
           text: "Login",
           shape: "pill",
           color: "white",
-          locale: "en"
+          locale: "en",
         }
       );
-  
-      google.accounts.id.renderButton(
-        document.getElementById("signInMain"),
-        {
-          theme: "outline",
-          size: "standard",
-          text: "Login",
-          shape: "pill",
-          color: "white",
-          locale: "en"
-        }
-      );
+
+      google.accounts.id.renderButton(document.getElementById("signInMain"), {
+        theme: "outline",
+        size: "standard",
+        text: "Login",
+        shape: "pill",
+        color: "white",
+        locale: "en",
+      });
     } else {
       window.location.reload();
       console.error("Google Sign-In script not loaded");
     }
   }, [handleCallbackResponse]);
-  
+
   const toggleSidebar = () => {
     setIsSidebarVisible(!isSidebarVisible);
   };
@@ -68,9 +68,13 @@ const Navbar = () => {
     <>
       <nav className="navbar">
         <div className={`nav-items sidebar`}>
-          <ul className={`${isSidebarVisible ? 'visible' : ''}`}>
+          <ul className={`${isSidebarVisible ? "visible" : ""}`}>
             <span>
-              <li><a onClick={toggleSidebar}><i className="ri-close-fill"></i></a></li>
+              <li>
+                <a onClick={toggleSidebar}>
+                  <i className="ri-close-fill"></i>
+                </a>
+              </li>
             </span>
             <li>
               <Link to="/">Home</Link>
@@ -83,17 +87,24 @@ const Navbar = () => {
             </li>
             <li>
               <div id="signInSidebar"></div>
-              {user.picture &&
+              {user.picture && (
                 <div>
-                  <img className="circle-image" src={user.picture} onClick={ (e) => handleSignOut(e) } alt="Profile"></img>
+                  <img
+                    className="circle-image"
+                    src={user.picture}
+                    onClick={(e) => handleSignOut(e)}
+                    alt="Profile"
+                  ></img>
                 </div>
-              }
+              )}
             </li>
           </ul>
         </div>
         <div className="nav-items">
           <ul>
-            <li><img src={imgLogo} alt="Logo" className="logo"/></li>
+            <li>
+              <img src={imgLogo} alt="Logo" className="logo" />
+            </li>
             <div className="nav__link">
               <li>
                 <Link to="/">Home</Link>
@@ -106,20 +117,29 @@ const Navbar = () => {
               </li>
               <li>
                 <div id="signInMain"></div>
-                {user.picture &&
+                {user.picture && (
                   <div>
-                    <img className="circle-image" src={user.picture} onClick={ (e) => handleSignOut(e) } alt="Profile"></img>
+                    <img
+                      className="circle-image"
+                      src={user.picture}
+                      onClick={(e) => handleSignOut(e)}
+                      alt="Profile"
+                    ></img>
                   </div>
-                }
+                )}
               </li>
             </div>
-            <li><a onClick={toggleSidebar} id="toggle"><i className="ri-menu-fill"></i></a></li>
+            <li>
+              <a onClick={toggleSidebar} id="toggle">
+                <i className="ri-menu-fill"></i>
+              </a>
+            </li>
           </ul>
         </div>
       </nav>
       <Outlet />
     </>
   );
-}
+};
 
 export default Navbar;
