369f6cfc1b06fbefd5a6d60b1b614f0fc33b20df
Aaron Hartono <aaronhartono28@gmail.com>
2024-02-06 20:27:39 +0700

    update

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://charity.cccup.id/',
       changeOrigin: true
     })
   );
